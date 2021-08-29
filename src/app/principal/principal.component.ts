87c8d6b45c2b64ed0e00a2ccde97e5925e0d7c08
Karenvs25 <karvillafrancosibrian@gmail.com>
2021-08-28 20:27:13 -0600

    Primer click

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { estudiante } from '../entidades/estudiante';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
@@ -11,7 +12,7 @@ export class PrincipalComponent implements OnInit {
   usuario:estudiante= {} as estudiante;
   usuarios:any;
 
-  constructor(private http:HttpClient) { 
+  constructor(private http:HttpClient,private router:Router) { 
     http.get('https://frozen-meadow-48728.herokuapp.com/todos')
     .subscribe(response=>{
       this.usuarios=response;
@@ -24,7 +25,29 @@ export class PrincipalComponent implements OnInit {
     this.http.post<estudiante>('https://frozen-meadow-48728.herokuapp.com/registrar',this.usuario)
     .subscribe(response=>{
       console.log(response);
+      this.usuario= {} as estudiante;
+      window.location.reload();
     });
+  }
+
+  editar(id:number):void{
+    console.log("el id es igual a = "+id);g
+    this.http.get<estudiante>('https://frozen-meadow-48728.herokuapp.com/uno/'+id)
+    .subscribe(response=>{
+      this.usuario=response;
+    })
+  
+  }
+
+  eliminar(id:number):void{
+    console.log("el id es igual a = "+id);
+    this.http.delete<estudiante>('https://frozen-meadow-48728.herokuapp.com/eliminar/'+id)
+    .subscribe(response=>{
+     // this.usuario=response;
+     alert('Usuario eliminado');
+     window.location.reload();
+    });
+  
   }
 
   ngOnInit(): void {
